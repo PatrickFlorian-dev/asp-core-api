8ec8eae6926d128fe5b3e4fc77d72e921fe3787b
PatrickFlorian-dev <patrickfloriansoftwareengineer@gmail.com>
2021-03-08 01:48:48 -0500

    Optimized objects , error handling, etc

---
+++ BASE/Models/HttpResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BASE.Models
+{
+    public class HttpResult
+    {
+        public bool success { get; set; }
+        public object? data { get; set; }
+        public string message { get; set; }
+    }
+}
--- LOGIC/UserLogic/UserLogic.cs
+++ LOGIC/UserLogic/UserLogic.cs
@@ -1,4 +1,5 @@
-﻿using core_api.Models;
+﻿using BASE.Models;
+using core_api.Models;
 using DAL.Entities;
 using DAL.Functions;
 using DAL.Interfaces;
@@ -13,33 +14,61 @@ namespace LOGIC.UserLogic
     {
         private IUser _user = new UserFunctions();
 
+        private HttpResult httpResult = new HttpResult();
+
         // Add a new user
-        public async Task<Boolean> CreateNewUser(UserViewModel userObj)
+        public async Task<HttpResult> CreateNewUser(UserViewModel userObj)
         {
             try
             {
                 var result = await _user.AddUser(userObj);
                 if (result.Id > 0)
                 {
-                    return true;
+                    httpResult.success = true;
+                    httpResult.data = result;
+                    httpResult.message = "Succesfully added user";
+                    return httpResult;
                 }
                 else
                 {
-                    return false;
+                    httpResult.success = false;
+                    httpResult.data = result;
+                    httpResult.message = "Failed to add user, Username/Email exists";
+                    return httpResult;
                 }
             }
             catch (Exception error)
             {
-                Console.Write(error);
-                return false;
+                return new HttpResult { success = false, data = error, message = "API failed - CreateNewUser" };
             }
         }
 
         // Get all users
-        public async Task<List<User>> GetAllUsers()
+        public async Task<HttpResult> GetAllUsers()
         {
-            List<User> users = await _user.GetAllUsers();
-            return users;
+            List<UserViewModel> users = await _user.GetAllUsers();
+
+            try
+            {
+                if (users.Count > 0)
+                {
+                    httpResult.success = true;
+                    httpResult.data = users;
+                    httpResult.message = "Succesfully retreive all users, Count: " + users.Count.ToString();
+                    return httpResult;
+                }
+                else
+                {
+                    httpResult.success = false;
+                    httpResult.data = users;
+                    httpResult.message = "Failed to retreive all usersr";
+                    return httpResult;
+                }
+            }
+            catch (Exception error)
+            {
+                return new HttpResult { success = false, data = error, message = "API failed - GetAllUsers" };
+            }
         }
 
     }
